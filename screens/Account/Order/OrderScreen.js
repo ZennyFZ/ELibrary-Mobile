5eb52ad0a8e169ba75f2134a5f17e380b3a8779a
Zenny <nhandinhdo291@gmail.com>
2024-02-27 13:17:52 +0700

    OrderScreen + login fix

--- screens/Account/Order/OrderScreen.js
+++ screens/Account/Order/OrderScreen.js
@@ -1,9 +1,55 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { getOrderByUserId } from "../../../apis/OrderService";
+import { retrieveData } from "../../../utils/AsyncStorage";
+import styles from "./Style";
+import { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import Ionicons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const OrderScreen = () => {
+    const [orders, setOrders] = useState([]);
+    const navigation = useNavigation();
+
+    const getUserOrders = async () => {
+        try {
+            const userId = await retrieveData("userId");
+            const orderData = await getOrderByUserId(userId)
+            setOrders(orderData.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getUserOrders();
+    }, []);
+
     return (
-        <View>
-            <Text>OrderScreen</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigation.navigate("Account")} style={styles.goBack}>
+                    <Ionicons name="arrow-left" size={30} color="#000" />
+                </TouchableOpacity>
+                <Text style={styles.headerTitle}>Orders</Text>
+            </View>
+            <FlatList
+                data={orders}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate("OrderDetail", { orderId: item._id, orderDate: item.orderDate })}>
+                        <View style={styles.orderContainer}>
+                            <View style={styles.orderIDDateBox}>
+                                <Text style={styles.orderId}>Order#: {item._id}</Text>
+                                <Text style={styles.orderDate}>Date: {item.createdAt.split('T')[0]}</Text>
+                            </View>
+                            <View style={styles.orderPricePaymentBox}>
+                                <Text style={styles.orderPrice}>{item.totalPrice}đ</Text>
+                                <Text style={styles.orderPayment}>Payment: {item.paymentMethod}</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item._id}
+            />
         </View>
     );
 }
--- screens/Account/Order/Style.js
+++ screens/Account/Order/Style.js
@@ -4,7 +4,145 @@ const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: "#dfe1e6",
+        padding: 10,
+    },
+    header: {
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    goBack: {
+        width: 40,
+    },
+    headerTitle: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    orderContainer: {
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "#fff",
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 10,
+    },
+    orderIDDateBox: {
+        padding: 5,
+    },
+    orderPricePaymentBox: {
+        flexDirection: "row",
+        padding: 5,
+    },
+    orderId: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    orderDate: {
+        fontSize: 20,
+        color: "#7f8c8d",
+    },
+    orderPrice: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#1435C9",
+    },
+    orderPayment: {
+        fontSize: 20,
+        position: "absolute",
+        right: 0,
+        top: 5,
+        color: "#2ECC71",
+    },
+    orderDetailContainer: {
+        flex: 1,
+        backgroundColor: "#E67E22",
+        padding: 10,
+        alignItems: "center",
+    },
+    orderDetailHeader: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        height: 180,
+    },
+    goBackDetail: {
+        position: "absolute",
+        left: 10,
+        top: 10,
+    },
+    headerTitleDetail: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "white",
+    },
+    orderDetailIDDateBox: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        height: 150,
+        width: '90%',
+        borderRadius: 10,
+        padding: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    orderDetailIDDateBoxTitle: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: "black",
+    },
+    orderDetailIDDateBoxSubTitle: {
+        fontSize: 20,
+        color: "grey",
+    },
+    orderDetailImage: {
+        width: windowWidth / 3,
+        height: windowHeight / 4,
+    },
+    orderDetailTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    orderDetailPrice: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#2ECC71",
+    },
+    orderDetailItemBox: {
+        flexDirection: "column",
+        backgroundColor: "#fff",
+        width: windowWidth,
+        marginTop: 10,
+        borderRadius: 10,
+    },
+    orderDetailItemImage: {
+        width: windowWidth / 3,
+        height: windowHeight / 4,
+    },
+    orderDetailItemTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    orderDetailItemPrice: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#2ECC71",
+    },
 })
 
 export default styles;
