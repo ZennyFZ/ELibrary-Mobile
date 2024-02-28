079f8a2cd826756c76afe1630f68b6dfe9e7cf51
Zenny <nhandinhdo291@gmail.com>
2024-02-28 15:00:38 +0700

    BookViewerScreen

--- screens/MyBook/BookViewerScreen.js
+++ screens/MyBook/BookViewerScreen.js
@@ -1,23 +1,42 @@
-import { Alert, Text, View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { WebView } from 'react-native-webview';
-import styles from "./Style";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import Ionicons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const BookViewerScreen = ({ route }) => {
+    const [pdfURL, setPdfURL] = useState("");
     const navigation = useNavigation();
-    const bookFileUrl = "";
+
+    const webViewRef = useRef();
+
+    useFocusEffect(
+        useCallback(() => {
+            webViewRef.current.reload();
+            setPdfURL(`https://docs.google.com/viewer?url=${route.params.pdfLink}&embedded=true`);
+        }, [route.params.pdfLink])
+    );
+
+    useEffect(() => {
+        webViewRef.current.reload();
+        setPdfURL(`https://docs.google.com/viewer?url=${route.params.pdfLink}&embedded=true`);
+    }, [])
+    
 
     return (
-        <View>
-            {Alert.alert(
-                "Alert",
-                "Not supported yet!",
-                [
-                    { text: "OK", onPress: () => navigation.navigate("MyBook") }
-                ]
-            )}
+        <View style={{ flex: 1 }}>
+            <TouchableOpacity onPress={() => { navigation.navigate("MyBook"), setPdfURL("") }}>
+                <Ionicons name="arrow-left" size={30} color="black" style={{ padding: 10, marginTop: 30 }} />
+            </TouchableOpacity>
+            <WebView
+                source={{ uri: pdfURL }}
+                style={{ marginTop: 20 }}
+                bounces={false}
+                ref={(ref) => (webViewRef.current = ref)}
+                cacheEnabled={false}
+            />
         </View>
-    )
+    );
 }
 
 export default BookViewerScreen;
