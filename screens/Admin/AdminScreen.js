6891e55930baa0fd7acaec8454d2ba1f32079f09
PenVsse <vusiphu20042000@gmail.com>
2024-03-05 17:32:27 +0700

    admin manage

--- screens/Admin/AdminScreen.js
+++ screens/Admin/AdminScreen.js
@@ -1,12 +1,33 @@
-import { Text, View } from "react-native";
-import styles from "./Style";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import CategoryManageScreen from "./CategoryManage/CategoryManageScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import BookManageScreen from "./BookManage/BookManageScreen";
 
-const Admin = () => {
-    return (
-        <View>
-            <Text>Admin</Text>
-        </View>
-    );
-}
+const Drawer = createDrawerNavigator();
+
+const Admin = ({ navigation }) => {
+  const drawerOption = {
+    unmountOnBlur: true,
+    headerTitleAlign: "center",
+    headerRight: () => (
+      <Ionicons
+        name="arrow-back-outline"
+        size={20}
+        color="black"
+        style={{ marginRight: 10 }}
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    ),
+  };
+
+  return (
+    <Drawer.Navigator initialRouteName="BookManage">
+      <Drawer.Screen name="BookManage" component={BookManageScreen} options={{ ...drawerOption, headerTitle: "Book Manage" }} />
+      <Drawer.Screen name="CategoryManage" component={CategoryManageScreen} options={{ ...drawerOption, headerTitle: "Category Manage" }} />
+    </Drawer.Navigator>
+  );
+};
 
 export default Admin;
